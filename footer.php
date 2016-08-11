e4caa7a2b88aa49a934130e94830a417b131167e
Lance Cleveland <lance@storelocatorplus.com>
2016-08-11 09:01:39 -0400

    Link to a new 'cyber_sprocket' window.

--- footer.php
+++ footer.php
@@ -13,9 +13,10 @@
 	<footer id="colophon" role="contentinfo">
 		<div class="site-info">
 			<?php do_action( 'twentytwelve_credits' ); ?>
-			<a href="<?php echo esc_url( __( 'http://www.cybersprocket.com/', 'twentytwelve-docsite' ) ); ?>"
-               title="<?php printf( __( 'A %s Derivative Work.', 'twentytwelve-docsite' ), 'Cyber Sprocket Labs' ); ?>"
-               ><?php printf( __( 'A %s Derivative Work.', 'twentytwelve-docsite' ), 'Cyber Sprocket Labs' ); ?></a>
+			<a  target="cyber_sprocket"
+				href="<?php echo esc_url( __( 'http://www.cybersprocket.com/', 'twentytwelve-docsite' ) ); ?>"
+               title="<?php printf( __( 'A %s Derivative Work', 'twentytwelve-docsite' ), 'Cyber Sprocket Labs' ); ?>"
+               ><?php printf( __( 'A %s Derivative Work', 'twentytwelve-docsite' ), 'Cyber Sprocket Labs' ); ?></a>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
