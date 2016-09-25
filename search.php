0636e0db376e3b5c38e46c831a30d580a9a502ab
Lance Cleveland <lance@storelocatorplus.com>
2016-09-25 12:34:37 -0400

    Take over the parent theme search form.

---
+++ search.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * The template for displaying the search
+ *
+ * Contains search content
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve_Docsite
+ * @since Twenty Twelve Docsite 4.4.01
+ */
+
+get_header(); ?>
+
+	<section id="primary" class="site-content">
+		<div id="content" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<header class="page-header">
+				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentytwelve-docsite' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+			</header>
+
+			<?php twentytwelve_content_nav( 'nav-above' ); ?>
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'content', get_post_format() ); ?>
+			<?php endwhile; ?>
+
+			<?php twentytwelve_content_nav( 'nav-below' ); ?>
+
+		<?php else : ?>
+
+			<article id="post-0" class="post no-results not-found">
+				<header class="entry-header">
+					<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentytwelve-docsite' ); ?></h1>
+				</header>
+
+				<div class="entry-content">
+					<p><?php _e( 'Nothing matched your search criteria. Please try again with different keywords.', 'twentytwelve-docsite' ); ?></p>
+					<?php get_search_form(); ?>
+				</div><!-- .entry-content -->
+			</article><!-- #post-0 -->
+
+		<?php endif; ?>
+
+		</div><!-- #content -->
+	</section><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
