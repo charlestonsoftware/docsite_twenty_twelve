a2f129702240376830778a541d9a207442ab55ae
Lance Cleveland <lance@storelocatorplus.com>
2016-09-25 14:42:15 -0400

    Drop the double footer output.

--- functions.php
+++ functions.php
@@ -69,28 +69,6 @@ if ( ! function_exists( 'twentytwelve_docsite_entry_meta' ) ) :
 			esc_html( get_the_date() )
 		);
 
-		$author = sprintf( '<span class="author vcard"><a class="url fn n" href="%1$s" title="%2$s" rel="author">%3$s</a></span>',
-			esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-			esc_attr( sprintf( __( 'View all posts by %s', 'twentytwelve-docsite' ), get_the_author() ) ),
-			get_the_author()
-		);
-
-		// Translators: 1 is category, 2 is tag, 3 is the date and 4 is the author's name.
-		if ( $tag_list ) {
-			$utility_text = __( 'This entry was posted in %1$s and tagged %2$s on %3$s<span class="by-author"> by %4$s</span>.', 'twentytwelve-docsite' );
-		} elseif ( $categories_list ) {
-			$utility_text = __( 'This entry was posted in %1$s on %3$s<span class="by-author"> by %4$s</span>.', 'twentytwelve-docsite' );
-		} else {
-			$utility_text = __( 'This entry was posted on %3$s<span class="by-author"> by %4$s</span>.', 'twentytwelve-docsite' );
-		}
-
-		printf(
-			$utility_text,
-			$categories_list,
-			$tag_list,
-			$date,
-			$author
-		);
 		?>
 		<div class="post_meta">
 			<h3><?php echo __('About This Article' , 'twentytwelve-docsite');?></h3>
