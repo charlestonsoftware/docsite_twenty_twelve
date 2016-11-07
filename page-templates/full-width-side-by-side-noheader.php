f776f1f1f59a05d4a5025b8871edc938da07fa87
Lance Cleveland <lance@storelocatorplus.com>
2016-11-07 11:57:04 -0500

    Update our new template.

--- page-templates/full-width-side-by-side-noheader.php
+++ page-templates/full-width-side-by-side-noheader.php
@@ -10,17 +10,4 @@
  * @since Twenty Twelve Docsite 4.6.1
  */
 
-get_header(); ?>
-
-	<div id="primary" class="site-content">
-		<div id="content" role="main">
-
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content', 'page' ); ?>
-				<?php comments_template( '', true ); ?>
-			<?php endwhile; // end of the loop. ?>
-
-		</div><!-- #content -->
-	</div><!-- #primary -->
-
-<?php get_footer(); ?>
+include_once( 'full-width-noheader.php'); ?>
